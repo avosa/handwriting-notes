3d1e8810f690cc0db1d1952535b387f4806541c4
avosa <avosa.developer@gmail.com>
2026-07-08 19:43:52 -0700

    add export and lint libraries and embed static fonts
    - pdf-lib, fontkit, docx for export; typescript-eslint, globals for lint
    - drop the concurrently dependency now that there is no server process
    - embed Caveat and Indie Flower as woff2 for screen and static ttf for export

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,18 @@ import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import vue from 'eslint-plugin-vue'
 import prettier from '@vue/eslint-config-prettier'
+import globals from 'globals'
 
 export default ts.config(
   { ignores: ['dist', 'node_modules', 'playwright-report', 'test-results'] },
   js.configs.recommended,
   ...ts.configs.recommended,
   ...vue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
   {
     files: ['**/*.vue'],
     languageOptions: {
